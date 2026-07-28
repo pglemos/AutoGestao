b1495b037bdf5ff666a39cc9b35298171ed6e689
Pedro Guilherme Lemos Teixeira <pglemos31@gmail.com>
2026-07-28 03:21:09 -0300

    chore(db): remove superseded local rollback version

--- supabase/rollbacks/20260728050000_deterministic_actions_runtime_and_security.sql
+++
@@ -1,9 +0,0 @@
--- DOWN
-BEGIN;
-
-DROP TABLE IF EXISTS public.deterministic_action_resolutions;
-
-GRANT EXECUTE ON FUNCTION public.prevent_valor_negociado_tamper_after_close()
-  TO PUBLIC, anon, authenticated, postgres, service_role;
-
-COMMIT;
